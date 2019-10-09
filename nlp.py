793292471d5788d872c0947c54a672fab2d2f373
Spyros Ntouroukis <spyros-ntouroukis@hotmail.com>
2019-10-10 01:57:58 +0300

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -5,23 +5,29 @@ from nltk.corpus import stopwords
 import string
 import re
 from timeit import default_timer as timer
+from nltk.stem import PorterStemmer
+
+
+#https://medium.com/@pemagrg/pre-processing-text-in-python-ad13ea544dae
 start = timer()
 import time
 job_cat_list = []
 query = "select meta_value from meta_terms where meta_key like 'description'"
 data=database.main(query)
-data = str(data)
-s2 = re.sub(r'<.*?>','',data)
 
-data6 = s2.replace("\\r", " ")
-data6 = data6.replace("\\xa0", "; ")
-data6 = data6.replace("\\r\\n", " ")
-data6 = data6.replace("\\r\\r", " ")
-data6 = data6.replace("\\n\\xa0", "; ")
-data6 = data6.replace("\\n\\n", " ")
-data6 = data6.replace("\\n", " ")
 
-nltk.download('all-corpora')
+def dataprocessing(data):
+    data = str(data)
+    s2 = re.sub(r'<.*?>','',data)
+    data6 = s2.replace("\\r", " ")
+    data6 = data6.replace("\\xa0", "; ")
+    data6 = data6.replace("\\r\\n", " ")
+    data6 = data6.replace("\\r\\r", " ")
+    data6 = data6.replace("\\n\\xa0", "; ")
+    data6 = data6.replace("\\n\\n", " ")
+    data6 = data6.replace("\\n", " ")
+    nltk.download('all-corpora')
+    return data6
 
 def remove_punctuation(text):
     '''a function for removing punctuation'''
@@ -30,30 +36,41 @@ def remove_punctuation(text):
     translator = str.maketrans('', '', string.punctuation)
     # return the text stripped of punctuation marks
     return text.translate(translator)
-data6 = remove_punctuation(data6)
-#print(sent_tokenize(data2,"Greek"))
-words = word_tokenize(data6)
-stop_words = set(stopwords.words('greek'))
 
-filtered_sentence = [w for w in words if not w in stop_words]
+def porterstemmer(word):
+    ps = PorterStemmer()
+    for word in words:
+        print(word + ":" + ps.stem(word))
+    return
+def plot(filtered_sentence):
+    freq = nltk.FreqDist(filtered_sentence)
+    for key, val in freq.items():
+        print(str(key) + ':' + str(val))
+    freq.plot(50, cumulative=False)
+
+
+def matrix(results):
+    end = timer()
+    lr = len(set(results)) / len(results)
+    count_distinct = len(set(results))
+    count_words_total = len(results)
+    print("count_distinct", count_distinct)
+    print("measure of the lexical richness of the text. The next example shows us that the number of distinct words",
+          lr * 100, "%")
+    print("Total words", count_words_total)
+    print("time:", end - start)
+
+
+
+data6 =dataprocessing(data)
+data6 = remove_punctuation(data6)
+sent_tokenize(data6,"greek")
+words = word_tokenize(data6)
+#words = porterstemmer(words)
+stop_words = set(stopwords.words('greek'))
 filtered_sentence = []
 for w in words:
     if w not in stop_words:
         filtered_sentence.append(w)
-
-end = timer()
-lr =len(set(filtered_sentence)) / len(filtered_sentence)
-count_distinct = len(set(filtered_sentence))
-count_words_total = len(filtered_sentence)
-print("count_distinct" , count_distinct)
-print("Distinct" , lr*100 , "%")
-print("Total words" , count_words_total)
-
-
-freq = nltk.FreqDist(filtered_sentence)
-for key,val in freq.items():
-    print(str(key) + ':' + str(val))
-freq.plot(50, cumulative=False)
-
-print("measure of the lexical richness of the text. The next example shows us that the number of distinct words" , lr *100)
-print("time:", end - start)
+matrix(filtered_sentence)
+plot(filtered_sentence)
